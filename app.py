115ed13ff0ca85280be622cc0a024c55aec72a68
rahuldecraj <rahuldecraj@gmail.com>
2026-03-17 17:12:20 +0530

    return msg change

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD with DockerHub Success 🚀"
+    return "CI/CD TEST 1"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)

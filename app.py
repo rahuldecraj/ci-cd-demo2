f22bb48668718891624c3552b9ad299a761d1018
rahuldecraj <rahuldecraj@gmail.com>
2026-03-18 16:08:05 +0530

    return updated

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, Response  # Added Response
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST # Added CONTENT_TYPE_LATEST
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "CI/CD TEST SUCCESS"
+# metric
+REQUEST_COUNT = Counter('request_count', 'Total Request Count')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+@app.route('/')
+def home():
+    REQUEST_COUNT.inc()
+    return "CI/CD TEST Success"
+
+@app.route('/metrics')
+def metrics():
+    # This wraps the metrics in a proper response with the correct headers
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
